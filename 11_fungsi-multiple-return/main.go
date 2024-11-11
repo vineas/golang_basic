97312a3f852e0328d102d72c4989b37990f82d66
vineas <alvienasyandika@gmail.com>
2024-11-11 20:03:41 +0700

    add multiple function

---
+++ 11_fungsi-multiple-return/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "math"
+
+func calculate(d float64) (float64, float64) {
+	// hitung luas
+	var area = math.Pi * math.Pow(d/2, 2)
+	// hitung keliling
+	var circumference = math.Pi * d
+
+	// kembalikan 2 nilai
+	return area, circumference
+
+	/* Fungsi dengan predefined return value */
+
+}
